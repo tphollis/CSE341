190e5ca4ad6b29d32c97b47410d56a6bf24ca5df
tphollis <thomas.preston.hollis@outlook.com>
2022-05-09 08:17:30 -0600

    made small update to allow requests to work.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -34,7 +34,7 @@ const createContact = async (req, res) => {
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    res.status(500).json(response.error || 'There was an error when creating the contact.');
   }
 };
 
@@ -57,7 +57,7 @@ const updateContact = async (req, res) => {
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    res.status(500).json(response.error || 'There was an error when updating the contact.');
   }
 };
 
@@ -68,7 +68,7 @@ const deleteContact = async (req, res) => {
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    res.status(500).json(response.error || 'There was an error when deleting the contact.');
   }
 };
 

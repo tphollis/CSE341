655c237fb5f76a6a34d74a23b13bdf000fb37522
tphollis <thomas.preston.hollis@outlook.com>
2022-05-14 13:15:28 -0600

    Created a Swagger documentation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
-router.use('/contacts', require('./contacts'))
+router.use('/contacts', require('./contacts'));
+router.use('/', require('./swagger'));
 
 module.exports = router;
